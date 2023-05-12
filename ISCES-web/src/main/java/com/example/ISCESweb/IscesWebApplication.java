9e2e08e58cf85997ab7b83f563cd1bdf63bbc481
Betül Soylek <betulsoylek2001@hotmail.com>
2023-05-12 22:38:57 +0300

    Initial commit

---
+++ ISCES-web/src/main/java/com/example/ISCESweb/IscesWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.ISCESweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IscesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IscesWebApplication.class, args);
+	}
+
+}
