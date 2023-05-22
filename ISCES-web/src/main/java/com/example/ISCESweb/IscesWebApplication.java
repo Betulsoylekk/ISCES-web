0c5edc4e76c26e1aa2b4f73a502e2f04c5ad44d8
aybilgehan <bilgehanay@outlook.com>
2023-05-22 17:02:47 +0300

    güncel

--- ISCES-web/src/main/java/com/example/ISCESweb/IscesWebApplication.java
+++ ISCES-web/src/main/java/com/example/ISCESweb/IscesWebApplication.java
@@ -1,13 +1,27 @@
 package com.example.ISCESweb;
 
+import com.example.ISCESweb.repositories.IDepartmentRepository;
+import com.example.ISCESweb.repositories.IFacultyRepository;
+import com.example.ISCESweb.repositories.IStudentRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class IscesWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IscesWebApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(IscesWebApplication.class, args);
+		var studentRepository = context.getBean(IStudentRepository.class);
+		var departmentRepository = context.getBean(IDepartmentRepository.class);
+		var facultyRepository = context.getBean(IFacultyRepository.class);
+
+
+		//System.out.println(student1.getDepartment());
+
+		System.out.println(studentRepository.findByDepartment_Faculty_FacultyName("ENG"));
+
 	}
 
 }
